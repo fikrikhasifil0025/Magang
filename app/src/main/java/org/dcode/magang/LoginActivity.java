c52c613058b7b5ba76ce44695846034b53c3aef5
hp <rsusanto27@gmail.com>
2018-01-25 10:27:53 +0700

    done

--- app/src/main/java/org/dcode/magang/LoginActivity.java
+++ app/src/main/java/org/dcode/magang/LoginActivity.java
@@ -1,7 +1,9 @@
 package org.dcode.magang;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,12 +11,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginButton;
     private EditText nik,password;
+    private TextView tvLupaPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +28,18 @@ public class LoginActivity extends AppCompatActivity {
         nik = (EditText) findViewById(R.id.input_nik);
         password = (EditText) findViewById(R.id.input_password);
         loginButton = (Button) findViewById(R.id.btn_login);
+        tvLupaPassword = (TextView) findViewById(R.id.lupa_password);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 login();
+            }
+        });
+        tvLupaPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lupaPassword();
             }
         });
     }
@@ -48,6 +59,28 @@ public class LoginActivity extends AppCompatActivity {
             AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
             builder.setMessage("Username atau Password Anda salah!")
                     .setNegativeButton("Retry", null).create().show();
+        }
+    }
+
+    public void lupaPassword(){
+        Context mContext = null;
+        String[] email = new String[]{
+                "khasifil10@gmail.com","faisalaulia10@gmail.com"
+        };
+
+        String mailSubject = "Lupa Password";
+        String mailBody = "Meminta penggantian password";
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.putExtra(Intent.EXTRA_EMAIL,email);
+        intent.putExtra(Intent.EXTRA_SUBJECT,mailSubject);
+        intent.putExtra(Intent.EXTRA_TEXT,mailBody);
+        intent.setData(Uri.parse("mailto:"));
+
+        if(intent.resolveActivity(getPackageManager())!= null){
+            startActivity(intent);
+        }else{
+            Toast.makeText(mContext, "Email Belum terdaftar", Toast.LENGTH_LONG);
         }
     }
 
--- app/src/main/java/org/dcode/magang/SplashScreen.java
+++ app/src/main/java/org/dcode/magang/SplashScreen.java
@@ -3,11 +3,13 @@ package org.dcode.magang;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
         Thread myThread = new Thread(){
